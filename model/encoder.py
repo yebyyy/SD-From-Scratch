0b210f01faef4b95cdd9621edb7f711ecc47046f
yebyyy <yexiaomeng2004@gmail.com>
2024-07-22 17:39:18 -0400

    Decreasing image size

--- model/encoder.py
+++ model/encoder.py
@@ -6,6 +6,44 @@ from decoder import VAE_AttentionBlock, VAE_ResidualBlock
 class VAE_Encoder(nn.Sequential):
 
     def __init__(self):
+        # Each pixel is representing more features and the number of pixels is decreasing
         super().__init__(
-            nn.Conv2d(3, 128, kernel_size=3, padding=1),
+            # (Batch_size, Channel, Height, Width) -> (Batch_size, 128, Height, Width)
+            nn.Conv2d(3, 128, kernel_size=3, padding=1),  # This won't change the size of the image
+
+            # (Batch_size, 128, Height, Width) -> (Batch_size, 128, Height, Width)
+            VAE_ResidualBlock(128, 128),
+
+            # (Batch_size, 128, Height, Width) -> (Batch_size, 128, Height, Width)
+            VAE_ResidualBlock(128, 128),
+
+            # (Batch_size, 128, Height, Width) -> (Batch_size, 128, Height / 2, Width / 2)
+            nn.Conv2d(128, 128, kernel_size=3, stride=2), 
+
+            # (Batch_size, 128, Height / 2, Width / 2) -> (Batch_size, 256, Height / 2, Width / 2)
+            VAE_ResidualBlock(128, 256),  # Increasing the number of features
+
+            # (Batch_size, 256, Height / 2, Width / 2) -> (Batch_size, 256, Height / 2, Width / 2)
+            VAE_ResidualBlock(256, 256),
+
+            # (Batch_size, 256, Height / 2, Width / 2) -> (Batch_size, 256, Height / 4, Width / 4)
+            nn.Conv2d(256, 256, kernel_size=3, stride=2), 
+
+            # (Batch_size, 256, Height / 4, Width / 4) -> (Batch_size, 512, Height / 4, Width / 4)
+            VAE_ResidualBlock(256, 512),
+            
+            # (Batch_size, 512, Height / 4, Width / 4) -> (Batch_size, 512, Height / 4, Width / 4)
+            VAE_ResidualBlock(512, 512),
+
+            # (Batch_size, 512, Height / 4, Width / 4) -> (Batch_size, 512, Height / 8, Width / 8)
+            nn.Conv2d(512, 512, kernel_size=3, stride=2),
+
+            VAE_ResidualBlock(512, 512),
+
+            VAE_ResidualBlock(512, 512),
+
+            # (Batch_size, 512, Height / 8, Width / 8) -> (Batch_size, 512, Height / 8, Width / 8)
+            VAE_ResidualBlock(512, 512),
+
+            VAE_AttentionBlock()
         )
