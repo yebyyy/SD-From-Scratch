4848852fb01bf9a165aed97877bd62c1b6ea66e4
yebyyy <yexiaomeng2004@gmail.com>
2024-07-26 17:00:41 -0400

    debug

--- model/diffusion.py
+++ model/diffusion.py
@@ -144,30 +144,30 @@ class UNET(nn.Module):
             # (Batch_size, 4, height / 8, width / 8)
             SwitchSequential(nn.Conv2d(4, 320, kernel_size=3, padding=1)),
                              
-            SwitchSequantial(UNET_ResidualBlock(320, 320), UNET_AttentionBlock(8, 40))
+            SwitchSequential(UNET_ResidualBlock(320, 320), UNET_AttentionBlock(8, 40)),
             
             # Decrease size of the image
             # (Batch_size, 4, height / 8, width / 8) -> (Batch_size, 320, height / 16, width / 16)
             SwitchSequential(nn.Conv2d(320, 320, kernel_size=3, stride=2, padding=1)),
                              
-            SwitchSequential(UNET_residualBlock(320, 640), UNET_AttentionBlock(8, 80)),
+            SwitchSequential(UNET_ResidualBlock(320, 640), UNET_AttentionBlock(8, 80)),
 
-            SwitchSequential(UNET_residualBlock(640, 640), UNET_AttentionBlock(8, 80)),
+            SwitchSequential(UNET_ResidualBlock(640, 640), UNET_AttentionBlock(8, 80)),
 
             # (batch_size, 640, height / 16, width / 16) -> (batch_size, 640, height / 32, width / 32)
             SwitchSequential(nn.Conv2d(640, 640, kernel_size=3, stride=2, padding=1)),
 
-            SwitchSequantial(UNET_ResidualBlock(640, 1280), UNET_AttentionBlock(8, 160)),
+            SwitchSequential(UNET_ResidualBlock(640, 1280), UNET_AttentionBlock(8, 160)),
 
-            SwitchSequantial(UNET_ResidualBlock(1280, 1280), UNET_AttentionBlock(8, 160)),
+            SwitchSequential(UNET_ResidualBlock(1280, 1280), UNET_AttentionBlock(8, 160)),
 
             # (batch_size, 1280, height / 32, width / 32) -> (batch_size, 1280, height / 64, width / 64)
-            SwitchSequential(nn.Conv2d(1280, 1280, kernel_size=3, stride=2, padding=1))
+            SwitchSequential(nn.Conv2d(1280, 1280, kernel_size=3, stride=2, padding=1)),
         
             SwitchSequential(UNET_ResidualBlock(1280, 1280)),
 
             # (batch_size, 1280, height / 64, width / 64)
-            SwitchSequential(UNET_ResidualBlock(1280, 1280))
+            SwitchSequential(UNET_ResidualBlock(1280, 1280)),
         ])
 
         self.bottleneck = SwitchSequential(
